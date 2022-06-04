883d465cfd85b786b6de9c3aa4510a6daac24c49
neizawa <meeteori@gmail.com>
2022-06-04 19:16:25 +0500

    Add button to change grid layout

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ for (let i = 0; i < 16; i++) {
     row.classList.add('row')
     container.appendChild(row);
 }
-
 const rows = Array.from(document.querySelectorAll('.row'));
-
 rows.forEach(row => {
     for (let i = 0; i < 16; i++) {
         const square = document.createElement('div');
@@ -15,3 +13,40 @@ rows.forEach(row => {
     }
 });
 
+const button = document.querySelector('button');
+button.addEventListener('click', createGrid);
+
+let number = 0;
+
+function promptNumber() {
+    number = prompt('Choose the number of squares per side for the new grid.', '')
+
+    if (number > 100)
+        promptNumber();
+    else if (number === '')
+        promptNumber();
+    else if (number === null)
+        return 16;
+    else 
+        return number;
+};
+
+function createGrid() {
+    number = promptNumber();
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    };
+
+    for (let i = 0; i < number; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row')
+        container.appendChild(row);
+    }
+    const rows = Array.from(document.querySelectorAll('.row'));
+    rows.forEach(row => {
+        for (let i = 0; i < number; i++) {
+            const square = document.createElement('div');
+            row.appendChild(square);
+        }
+    });
+};
